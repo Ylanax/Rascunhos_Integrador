f73e7e1a18fff67cc2e08e6b021999b7b565ac83
Ylana Pereira <ylana.pereira2004@outlook.com>
2025-06-24 21:59:57 -0300

    tentativa de conexao bd

---
+++ cabecalho.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>A Arte da Guerra</title>
+    <link rel="stylesheet" href="estilo.css">
+
+</head>
+
+<body id="fundo">
+    <main class="conteudo">
+        <header class="cabecalho1">
+            <a class="logo-texto1">Biblioteca Virtual</a>
+        </header>
---
+++ livros.php
@@ -0,0 +1,72 @@
+<?php
+include "cabecalho.php";
+?>
+
+<?php
+$servidor = 'localhost';
+$bd = 'projeto biblioteca';
+$usuario = 'root';
+$senha = '';
+
+$conexao = mysqli_connect($servidor, $usuario, $senha, $bd);
+if (!$conexao) {
+    die("deu ruim" . mysqli_connect_error());
+}
+
+$sql = "select * from livros";
+$resultado = mysqli_query($conexao, $sql);
+
+while ($linha = mysqli_fetch_assoc($resultado)) {
+?>
+    <div class="capalivro">
+        <img src="imagens/livro1.png" alt="Capa Livro A Arte Da Guerra" class="livroguerra">
+    </div>
+    <div class="descricaolivro">
+        <h1>
+            <span><?($linha['titulo']);?></span>
+            <span class="subtitulo">A Arte da Guerra</span>
+        </h1>
+        <h1>
+            <span class="titulo">Autor:</span>
+            <span class="subtitulo">Sun Tzu</span>
+        </h1>
+        <h1>
+            <span class="titulo">Instituição:</span>
+            <span class="subtitulo">Cultura Brasil</span>
+        </h1>
+        <h1>
+            <span class="titulo">Ano:</span>
+            <span class="subtitulo">2010 - 1ªEdição</span>
+        </h1>
+        <h1>
+            <span class="titulo">N° de Páginas:</span>
+            <span class="subtitulo">058</span>
+        </h1>
+        <h1>
+            <span class="titulo">Tipo:</span>
+            <span class="subtitulo">Livro Digital</span>
+        </h1>
+        <h1>
+            <span class="titulo">Formato:</span>
+            <span class="subtitulo">.pdf</span>
+        </h1>
+        <h1>
+            <span class="titulo">Licença:</span>
+            <span class="subtitulo">Dominio Público</span>
+        </h1>
+        <h1>
+            <span class="titulo2">Descrição</span><br>
+            <span class="subtitulo2">Sun Tzu, um filósofo e general nascido no Estado de Ch’i na China por volta de 500 a.C., escreveu "A Arte da Guerra". Este clássico da estratégia militar oferece ensinamentos aplicáveis tanto em contextos de guerra quanto em situações cotidianas. A vitória na guerra depende de cinco fatores: doutrina, tempo, terreno, comando e disciplina. Os princípios da guerra se baseiam no engano, sendo crucial conhecer o inimigo e a si mesmo para vencer, utilizando táticas e manobras com ataques diretos e indiretos para confundir o inimigo.</span>
+        </h1>
+        <a href="livro2/livro2.html">
+            <button type="button" class="botao-voltar2">Voltar</button><br>
+        </a>
+    </div>
+<?php
+}
+?>
+</main>
+
+</body>
+
+</html>
