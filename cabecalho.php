2f0a707cb2d459e26d7f3ae2acefabaec8bfb7ed
Ylana Pereira <ylana.pereira2004@outlook.com>
2025-06-25 22:33:00 -0300

    teste

--- cabecalho.php
+++ cabecalho.php
@@ -1,16 +1,16 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>A Arte da Guerra</title>
+    <title>Página Inicial</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
     <link rel="stylesheet" href="estilo.css">
-
 </head>
-
-<body id="fundo">
+<body>
     <main class="conteudo">
-        <header class="cabecalho1">
-            <a class="logo-texto1">Biblioteca Virtual</a>
+        <header class="cabecalho">
+            <img src="imagens/logolivro.png" width="30">
+            <h2 class="logo-texto">Biblioteca Virtual</h2>
         </header>
